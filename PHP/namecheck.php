f17b0c8bd6d8437663e28aebb76ed15be495b6b4
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-10-13 01:30:26 +0600

    Add files via upload

---
+++ PHP/namecheck.php
@@ -0,0 +1,13 @@
+<?php
+    //print_s($_GET);
+    $username = $_REQUEST['username'];
+    $password = $_REQUEST['password'];
+
+    if($username == "" || $password == ""){
+        echo "null value";
+    }else if($username == $password){
+        echo "valid user";
+    }else{
+        echo "invalid username/password!";
+    }
+?>
