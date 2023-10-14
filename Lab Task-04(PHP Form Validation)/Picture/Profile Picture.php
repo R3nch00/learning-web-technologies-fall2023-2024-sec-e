bbd01df96a499bf3d645644e7571cf6ae8887993
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-10-14 13:19:48 +0600

    Add files via upload

---
+++ Lab Task-04(PHP Form Validation)/Picture/Profile Picture.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+        <title>Profile Pictue Form</title>
+    </head>
+    <body>
+        <form method="post" action="" enctype="">
+            <fieldset>
+                <legend>PROFILE PICTURE</legend>
+                <table>
+                    <tr>
+                        <td><img src="C:/xampp/htdocs/WT/Picture/snap.jpg" style="width: 100; height: 100;"></td>
+                    </tr>
+                    <tr>
+                        <td><input type="file" name="image" value=""</td>
+                    </tr>
+                </table>
+                <hr>
+                <table>
+                    <tr>
+                        <td>
+                            <td><input type="submit" name="" id="submit"</td>
+                        </td>
+                    </tr>
+                </table>
+            </fieldset>
+        </form>
+    </body>
+</html>
+
+<?php
+
+?>
