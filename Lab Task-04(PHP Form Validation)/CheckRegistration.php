5eaec8b9d931ecd36c9efcedac5997e7a66f3215
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-10-16 22:56:28 +0600

    Add files via upload

---
+++ Lab Task-04(PHP Form Validation)/CheckRegistration.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "PHP VALIDATION: ";
+echo '<br><br>';
+
+session_start();
+
+$name=$_POST['name'];
+$email=$_POST['email'];
+$username=$_POST['username'];
+$password=$_POST['password'];
+$gender=$_POST['gender'];
+$date=$_POST['date'];
+
+
+?>
