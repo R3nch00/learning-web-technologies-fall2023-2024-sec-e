457a58e585dfb335284d3af1d54933e232015000
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-10-17 13:39:12 +0600

    Add files via upload

---
+++ Lab Task-05(PHP Layout)/PublicHome.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <title>Home page of X-company</title>
+    </head>
+    <body>
+        <table border="2" width="600px" style="border-collapse: collapse; margin-top: 20px">
+        
+            <tr>
+            <td><img src="C:/xampp/htdocs/companyX/X-company.png" alt="X-company logo" style="width:100;"><a align="right" href="PublicHome.php">Home</a> | <a href="login.php">Login</a> | <a href="Registration.php">Registration</a></td>
+        </tr>
+
+        <tr>
+            <td style="padding-bottom: 100;"><h2 style="margin: 30;">Welcome to the xCompany</h2></td>
+        </tr>
+
+        <tr align="center">
+            <td>
+                <footer style="margin: 10;">Copytight &copy; 2017</footer>
+            </td>
+        </tr>
+        
+        </table>
+    </body>
+</html>
+
+<?php
+
+    session_start();
+
+?>
