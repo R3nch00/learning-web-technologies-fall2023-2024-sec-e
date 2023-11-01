4bdd96a58ad94ef227698f899bf686f0a6053e69
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-11-01 23:16:09 +0600

    Update adminHome.php

--- Mid Lab Exam/Views/adminHome.php
+++ Mid Lab Exam/Views/adminHome.php
@@ -13,9 +13,9 @@ session_start();
         <h1><center>Welcome Bob!</center></h1>
        
         <center><a href="profile.php">Profile</a></center>
-        <center><a href="changepassword.php">Change Password</a></center>
-        <center><a href="userlist.php">View Users</a></center>
-        <center><a href="../controllers/logout.php">Logout</a></center>
+        <center><a href="changePassword.php">Change Password</a></center>
+        <center><a href="userList.php">View Users</a></center>
+        <center><a href="../controllers/logOut.php">Logout</a></center>
 
     </body>
 
