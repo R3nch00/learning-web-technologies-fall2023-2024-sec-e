fdfcb5b8ec4ef5668ddf2264501e412fc20de2fb
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-11-01 17:11:06 +0600

    Add files via upload

---
+++ Mid Lab Exam/Views/adminHome.php
@@ -0,0 +1,22 @@
+<?php
+
+session_start();
+
+?>
+
+<html>
+    <head>
+        <title>Admin Home</title>
+    </head>
+    <body>
+        
+        <h1><center>Welcome Bob!</center></h1>
+       
+        <center><a href="profile.php">Profile</a></center>
+        <center><a href="changepassword.php">Change Password</a></center>
+        <center><a href="userlist.php">Views Users</a></center>
+        <center><a href="../controllers/logout.php">Logout</a></center>
+
+    </body>
+
+</html>
