92358ad0f4fac22e6dcfe3399f16303c91450f3b
Omar Faysal <105789009+R3nch00@users.noreply.github.com>
2023-11-20 16:37:34 +0600

    Add files via upload

---
+++ Lab Task-08[Ajax]/JavaScript/script.js
@@ -0,0 +1,14 @@
+function ajax(){
+    var x = document.getElementById("ename").value;
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.open('POST', '../Controller/searhCheck.php', true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            document.getElementById('show').innerHTML = this.responseText
+        }
+    }
+
+    xhttp.send('ename='+x);
+}
